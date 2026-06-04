f070de933bb7ffa895c450da76be8c6cbe2e4996
zhoutao428 <z17756037070@gmail.com>
2026-06-04 17:18:30 +0800

    fix: xiaohongshu-publish params 加 TemplateParamDef 类型标注

--- src/lib/automation/fp-templates/xiaohongshu-publish.ts
+++ src/lib/automation/fp-templates/xiaohongshu-publish.ts
@@ -8,7 +8,7 @@
  *   - caption: 笔记标题+正文内容
  */
 
-import { TemplateResult, LogFn, FingerprintTemplate } from './types'
+import { TemplateResult, LogFn, FingerprintTemplate, TemplateParamDef } from './types'
 
 const meta = {
   key: 'xiaohongshu-publish',
@@ -18,7 +18,7 @@ const meta = {
   version: '1.0.0',
 }
 
-const params = [
+const params: TemplateParamDef[] = [
   {
     key: 'caption',
     label: '笔记内容',
